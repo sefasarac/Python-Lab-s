43fa97b9824aea86504e12d9e4d2c345a9b42680
Sefa Saraç <70567964+sefasarac@users.noreply.github.com>
2022-12-09 02:59:50 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,27 @@
+N = int(input())
+sum = 0 
+
+
+while (N>0):
+
+    if(N==1):
+        N = int(input())
+        continue
+
+    if(N==2):
+        sum +=2
+        N = int(input())
+        continue
+
+    for i in range(2,N):
+        if(N%i) == 0 :
+            break 
+            
+        else :
+            sum +=N
+            continue
+
+    N = int(input())
+
+print(sum)        
+    
